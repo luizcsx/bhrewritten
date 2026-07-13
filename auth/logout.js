defd0c575aae3356789c6bdb98901e6b1dea87c6
Luiz Miguel <contato.luiz404@gmail.com>
2026-07-13 17:19:58 -0300

    #075: Account log-out function

---
+++ auth/logout.js
@@ -0,0 +1,11 @@
+import { clearSessionCookie } from '../_lib/session.js';
+
+export async function onRequestPost() {
+    return new Response(null, {
+        status: 302,
+        headers: {
+            'Location':   '/',
+            'Set-Cookie': clearSessionCookie()
+        }
+    });
+}
